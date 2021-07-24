7eb7d958cbefa12b4623bf4415afc16d5ee8fab9
Rajat Rajput <rajputrajat@gmail.com>
2021-07-24 17:18:43 +0530

    derive macro

--- check/src/main.rs
+++ check/src/main.rs
@@ -1,6 +1,10 @@
-use proc_macro_lib::make_answer;
+use proc_macro_lib::{make_answer, AnswerFn};
+
+#[derive(AnswerFn)]
+struct Struct;
 
 fn main() {
     make_answer!();
     println!("{}", answer());
+    println!("{}", answer_derive());
 }
