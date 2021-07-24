8eff2d86190672c160774cfca42a0600676a2518
Rajat Rajput <rajputrajat@gmail.com>
2021-07-24 17:29:26 +0530

    wip. derive attr

--- check/src/main.rs
+++ check/src/main.rs
@@ -1,7 +1,17 @@
-use proc_macro_lib::{make_answer, AnswerFn};
+use proc_macro_lib::{make_answer, AnswerFn, HelperAttr};
 
 #[derive(AnswerFn)]
 struct Struct;
+
+#[derive(HelperAttr)]
+struct AttrStruct {
+    #[helper_1]
+    a: i32,
+    #[helper_2]
+    b: f32,
+    #[helper_3]
+    c: i8,
+}
 
 fn main() {
     make_answer!();
--- proc_macro_lib/src/lib.rs
+++ proc_macro_lib/src/lib.rs
@@ -9,3 +9,8 @@ pub fn make_answer(_item: TokenStream) -> TokenStream {
 pub fn derive_answer_fn(_item: TokenStream) -> TokenStream {
     "fn answer_derive() -> u32 { 42 }".parse().unwrap()
 }
+
+#[proc_macro_derive(HelperAttr, attributes(helper_1, helper_2, helper_3))]
+pub fn derive_attr_answer(_item: TokenStream) -> TokenStream {
+    "fn answer_derive_attr() -> u32 { 42 }".parse().unwrap()
+}
